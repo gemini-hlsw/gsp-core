a2710b017452e17295593940926c403ce3b66b49
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2020-05-07 14:16:05 -0400

    Updated to use gsp-sbt, requires changing Symbols
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- modules/gen/src/main/scala/gem/sql/enum/GsaoiEnums.scala
+++ modules/gen/src/main/scala/gem/sql/enum/GsaoiEnums.scala
@@ -16,7 +16,7 @@ object GsaoiEnums {
     List(
 
       EnumDef.fromQuery("GsaoiReadMode", "GSAOI Read Mode") {
-        type E = Record.`'tag -> String, 'shortName -> String, 'longName -> String, 'ndr -> Int, 'readNoise -> Int, 'minimumExposureTime -> FiniteDuration.Seconds, 'overhead -> FiniteDuration.Seconds`.T
+        type E = Record.`Symbol("tag")-> String, Symbol("shortName")-> String, Symbol("longName")-> String, Symbol("ndr")-> Int, Symbol("readNoise")-> Int, Symbol("minimumExposureTime")-> FiniteDuration.Seconds, Symbol("overhead")-> FiniteDuration.Seconds`.T
         sql"""SELECT id, id tag, short_name, long_name, ndr, read_noise, minimum_exposure_time, overhead FROM e_gsaoi_read_mode""".query[(String, E)]
       },
 
@@ -25,23 +25,23 @@ object GsaoiEnums {
         val  r = Witness(Symbol("GsaoiReadMode"))
         type M = m.T
         type R = r.T
-        type E = Record.`'tag -> String, 'shortName -> String, 'longName -> String, 'wavelength -> Wavelength.Um, 'readMode -> EnumRef[R], 'exposureTime5050 -> FiniteDuration.Seconds, 'exposureTimeHalfWell -> FiniteDuration.Seconds, 'band -> Option[EnumRef[M]]`.T
+        type E = Record.`Symbol("tag")-> String, Symbol("shortName")-> String, Symbol("longName")-> String, Symbol("wavelength")-> Wavelength.Um, Symbol("readMode")-> EnumRef[R], Symbol("exposureTime5050")-> FiniteDuration.Seconds, Symbol("exposureTimeHalfWell")-> FiniteDuration.Seconds, Symbol("band")-> Option[EnumRef[M]]`.T
         val ret = sql"""SELECT id, id tag, short_name, long_name, wavelength, read_mode_id, exposure_time_50_50, exposure_time_half_well, band_id FROM e_gsaoi_filter""".query[(String, E)]
         (ret, m.value: M, r.value: R)._1 // convince scalac that we really do use M and R
       },
 
       EnumDef.fromQuery("GsaoiUtilityWheel", "Gsaoi Utility Wheel") {
-        type E = Record.`'tag -> String, 'shortName -> String, 'longName -> String`.T
+        type E = Record.`Symbol("tag")-> String, Symbol("shortName")-> String, Symbol("longName")-> String`.T
         sql"""SELECT id, id tag, short_name, long_name FROM e_gsaoi_utility_wheel""".query[(String, E)]
       },
 
       EnumDef.fromQuery("GsaoiRoi", "Gsaoi Region of Interest") {
-        type E = Record.`'tag -> String, 'shortName -> String, 'longName -> String`.T
+        type E = Record.`Symbol("tag")-> String, Symbol("shortName")-> String, Symbol("longName")-> String`.T
         sql"""SELECT id, id tag, short_name, long_name FROM e_gsaoi_roi""".query[(String, E)]
       },
 
       EnumDef.fromQuery("GsaoiOdgwSize", "Gsaoi ODGW Size") {
-        type E = Record.`'tag -> String, 'pixels -> Int`.T
+        type E = Record.`Symbol("tag")-> String, Symbol("pixels")-> Int`.T
         sql"""SELECT id, id tag, pixels FROM e_gsaoi_odgw_pixels""".query[(String, E)]
       }
     )
